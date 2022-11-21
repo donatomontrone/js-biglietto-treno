3defd7bd3f9a14ed9f21f62a7f108b7038a8af05
donatomontrone <donato.montrone@outlook.com>
2022-11-21 15:50:05 +0100

    add comments

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+//Dichiarata variabile sui chilometri che si vogliono percorrere
 const userKmetres = parseInt(prompt("Inserisci i chilomentri da percorrere per il tuo viaggio."));
 
 console.log(userKmetres + "km");
 
+//Alert di verifica se i chilometri non sono numeri o sono eccessivi
 if (isNaN(userKmetres)) {
     alert("I chilometri devono essere espressi in numeri interi.");
     console.error("I chilometri devono essere espressi in numeri interi.");
@@ -10,8 +12,11 @@ if (isNaN(userKmetres)) {
     console.error("Limite chilometri superato.");
 }
 
+// Dichiarata variabile sull'età dell'utente
 const userAge = parseInt(prompt("Inserisci la tua età."));
 
+
+//Alert se l'età non è un numero o se troppo piccola/grande
 if (isNaN(userAge)) {
     alert("L'età deve essere espressa in numeri interi.")
     console.error("L'età deve essere espressa in numeri interi.")
@@ -22,6 +27,7 @@ if (isNaN(userAge)) {
 
 console.log(userAge + "anni")
 
+//Dichiarate variabili per il prezzo del biglietto e variabili per gli sconti
 let ticketPrice = userKmetres * 0.21;
 let ticketUnder
 let discountUnder;
@@ -29,6 +35,7 @@ let ticketOver;
 let discountOver;
 
 
+//Calcolo per applicare lo sconto se over 60 o se under 18
 if (userAge > 65){
     discountOver = ((ticketPrice * 40) / 100)
     ticketOver = ticketPrice - discountOver
@@ -37,7 +44,7 @@ if (userAge > 65){
     ticketUnder = ticketPrice - discountUnder
 }
 
-
+//Visualizzare nel file e nella console il prezzo finale in base allo sconto
 if ((userAge > 65) && (userAge <= 120)){
     document.getElementById("ticket-price").innerHTML = (ticketOver.toFixed(2) + '&euro;');
     console.log('Il prezzo del ticket scontato per over 65 è ' + ticketOver + 'euro');
@@ -48,6 +55,8 @@ if ((userAge > 65) && (userAge <= 120)){
     document.getElementById("ticket-price").innerHTML = (ticketPrice.toFixed(2) + '&euro;');
     console.log('Il prezzo del ticket è ' + ticketPrice + 'euro');
 }
+
+//alert di errore se l'età è sbagliata
 
 if ((userAge > 120) || (userAge < 1)){
     document.getElementById("ticket-price").innerHTML = ("ERRORE!!");
