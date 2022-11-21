6ff730cbe56f71dc0aa9e2a86f4bb1856a6caf91
donatomontrone <donato.montrone@outlook.com>
2022-11-21 17:57:24 +0100

    add content

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 //Dichiarata variabile sui chilometri che si vogliono percorrere
 const userKmetres = parseInt(prompt("Inserisci i chilomentri da percorrere per il tuo viaggio."));
 console.log(userKmetres + "km");
+document.getElementById('user-km').innerHTML = (userKmetres + ' km');
 
 // Dichiarata variabile sull'età dell'utente
 const userAge = parseInt(prompt("Inserisci la tua età."));
 console.log(userAge + "anni");
+document.getElementById('user-age').innerHTML = (userAge + ' anni');
 
 //Dichiarazione variabili per il prezzo al km
 let ticketPrice = userKmetres * 0.21;
 
 
-if (userAge < 18){
-    let discount = (ticketPrice - ((ticketPrice * 20 ) / 100))
-    document.getElementById('ticket-price').innerHTML = (discount.toFixed(2) + '&euro;');
+if ((userAge > 0) && (userAge < 18)){
+    ticketPrice = (ticketPrice - ((ticketPrice * 20 ) / 100))
+    document.getElementById('ticket-price-under').innerHTML = (ticketPrice.toFixed(2) + '&euro;');
+    console.log('Il prezzo del ticket scontato per minorenni è ' + ticketPrice + 'euro');
 
 } else if (userAge >= 65){
-    let discount = (ticketPrice - ((ticketPrice * 40 ) / 100))
-    document.getElementById('ticket-price').innerHTML = (discount.toFixed(2) + '&euro;');
+    ticketPrice = (ticketPrice - ((ticketPrice * 40 ) / 100))
+    document.getElementById('ticket-price-over').innerHTML = (ticketPrice.toFixed(2) + '&euro;');
+    console.log('Il prezzo del ticket scontato per maggiorenni è ' + ticketPrice + 'euro');
+    
 } else {
     document.getElementById("ticket-price").innerHTML = (ticketPrice.toFixed(2) + '&euro;');
+    console.log('Il prezzo del ticket è ' + ticketPrice + 'euro');
 }
